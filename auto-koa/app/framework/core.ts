4045e22aaadfe9cdcc39eddb6acf13efce7b6efc
brook <zhou@wdpe.net>
2022-10-12 18:03:23 +0800

    dev

--- auto-koa/app/framework/core.ts
+++ auto-koa/app/framework/core.ts
@@ -1,6 +1,35 @@
+import cluster from "cluster";
+const numCPUs = require("os").cpus().length;
+const koa = require("koa")
+const koaBody = require("koa-body")
+
 export function createServer(port: number, route: any, opt: { cors?: Accessheader }) {
-  const koa = require("koa")
-  const koaBody = require("koa-body")
+
+  if (cluster.isPrimary) {
+    for (var i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+    // 监听worker
+    cluster.on("listening", function (worker, address) {
+      console.log(
+        "listening: worker " +
+          worker.process.pid +
+          ", Address: " +
+          address.address +
+          ":" +
+          address.port +
+          "," +
+          address.addressType
+      );
+    });
+  
+    // 监听worker退出事件，code进程非正常退出的错误code，signal导致进程被杀死的信号名称
+    cluster.on("exit", function (worker, code, signal) {
+      console.log("工作进程 %d 关闭 (%s)(%s). 重启中...");
+  
+      cluster.fork();
+    });
+  } else {
 
   const app = new koa()
   app
@@ -16,4 +45,5 @@ export function createServer(port: number, route: any, opt: { cors?: Accessheade
     .use(route.routes())
     .use(route.allowedMethods())
     .listen(port)
+  }
 }
